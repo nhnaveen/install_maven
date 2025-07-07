b652bba5df11bc58474759a579a4ef0b8dac9526
nhnaveen <nhnaveen1994@gmail.com>
2025-07-07 12:17:30 +0530

    Update maven.sh

--- maven.sh
+++ maven.sh
@@ -10,19 +10,19 @@ MAVEN_TAR=$MAVEN_DIR-bin.tar.gz
 DOWNLOAD_URL=https://archive.apache.org/dist/maven/maven-3/3.6.3/binaries/apache-maven-3.6.3-bin.tar.gz
 
 
-echo "📦 Downloading Maven $MAVEN_VERSION..."
+echo "Downloading Maven $MAVEN_VERSION..."
 wget $DOWNLOAD_URL
 
-echo "📂 Extracting Maven..."
+echo "Extracting Maven..."
 tar -xvzf $MAVEN_TAR
 
-echo "🚚 Moving Maven to /opt/"
+echo "Moving Maven to /opt/"
 sudo mv $MAVEN_DIR /opt/
 
-echo "🔧 Configuring environment variables..."
+echo "Configuring environment variables..."
 echo "export M2_HOME=/opt/$MAVEN_DIR" >> ~/.bashrc
 echo "export PATH=\$M2_HOME/bin:\$PATH" >> ~/.bashrc
 source ~/.bashrc
 
-echo "✅ Maven installation complete!"
+echo "Maven installation complete!"
 mvn -version
